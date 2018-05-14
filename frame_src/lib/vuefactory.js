70285a72465410e76c704ae776132c5231dbe2e4
zhangfan6385 <zfcrocodile@sina.com>
2018-05-14 17:23:13 +0800

    commit template

--- frame_src/lib/vuefactory.js
+++ frame_src/lib/vuefactory.js
@@ -1,3 +1,4 @@
+//vue工厂，动态加载vue组件
 function vueFactory (component, parent) {  
   const constructor = Vue.extend(component)  
   let ins = new constructor({  
@@ -5,4 +6,4 @@ function vueFactory (component, parent) {
     parent: parent  
   })  
   window.document.body.appendChild(ins.$el)  
-}  
+} 
