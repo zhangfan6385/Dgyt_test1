b8c0eb2d947ff42521721613a6139083f4853aa8
jiaccool <jiaccool@163.com>
2018-06-25 13:48:23 +0800

    dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"https://api-dev"'
+  // BASE_API: '"https://api-dev"'
+  BASE_API:'"http://10.201.2.66:51332"'
 }
