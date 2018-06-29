f9fee2da76e501067e4964b5c4dc563b18263252
jiangqixin <jiangqixin0525@126.com>
2018-06-29 09:19:27 +0800

    测试数据

--- src/frame_src/mock/login.js
+++ src/frame_src/mock/login.js
@@ -8,7 +8,7 @@ const userMap = {
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
     name: 'Super Admin',
     sysCode: '1',
-    code: '2000',
+    code: 2000,
     userCode: 'admin',
     sysName: '大港软件开发平台',
     departCode: '1401',
