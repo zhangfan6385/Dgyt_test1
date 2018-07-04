04f554a475e2f770d5c1fe7a3a81c41c656433c4
zhangfan6385 <zfcrocodile@sina.com>
2018-07-04 08:49:40 +0800

    commit config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   // BASE_API: '"https://api-dev"'
   // BASE_API: '"http://192.168.1.122:51332"'
-  BASE_API: '"http://192.168.1.122:8188"'
+  BASE_API: '"http://localhost:12345"'
   // BASE_API: '"http://192.168.1.127:51332"'
 }
