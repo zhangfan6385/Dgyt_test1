1029b8f85c2b37f6ec57f35472ff453309cda3b5
zhangfan6385 <zfcrocodile@sina.com>
2018-07-05 20:59:33 +0800

    env 1

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,8 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   // BASE_API: '"https://api-dev"'
   // BASE_API: '"http://192.168.1.122:51332"'
-  BASE_API: '"http://10.201.1.82/API/"'
+  //BASE_API: '"http://10.201.1.82:8089/"'
+  //BASE_API: '"http://10.201.1.82/API/"'
+  BASE_API: '"http://192.168.140.120/"'
   // BASE_API: '"http://192.168.1.127:51332"'
 }
