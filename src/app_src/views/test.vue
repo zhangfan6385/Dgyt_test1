b32e35d97f64994ba33cf588360914d4f9032849
jiaccool <jiaccool@163.com>
2018-07-05 17:15:24 +0800

    baidu

--- src/app_src/views/test.vue
+++ src/app_src/views/test.vue
@@ -1,3 +1,22 @@
 <template>
-    <iframe src="www.baidu.com" frameborder="0" scrolling="auto" style="position:absolute;top: -300px;left: 0px;"></iframe>
+    <div>
+        <iframe id='iframePage' src="https://www.baidu.com/" frameborder="0" scrolling="auto" style="width:100%;height:800px;"></iframe>
+    </div>
 </template>
+
+<script>
+    export default {
+      methods: {
+        iframeLoad() {
+          console.log('cccc')
+          var idEle = document.getElementById('iframePage')
+          console.log(idEle)
+          // document.getElementById('iframePage').height = 0
+          // document.getElementById('iframePage').height = document.getElementById('iframe').contentWindow.document.body.scrollHeight
+        }
+      },
+      created() {
+        // this.iframeLoad()
+      }
+    }
+</script>
