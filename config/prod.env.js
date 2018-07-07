d1f898d756ac8ce823eb0528389658fe825c6001
zhangfan6385 <zfcrocodile@sina.com>
2018-07-07 09:39:14 +0800

    commit config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://10.201.1.82/API/"'
+  BASE_API: '"http://localhost/API/"'
   // BASE_API: '"http://10.201.1.82:12345"'
 }
