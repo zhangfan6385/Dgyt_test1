cc3b39f08b3a8b5b0af8d814082c3c48a6770e77
zhangfan6385 <zfcrocodile@sina.com>
2018-07-09 17:55:42 +0800

    env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://localhost/API/"'
+  //BASE_API: '"http://localhost/API/"'
+  BASE_API: '"http://localhost/UIDP_APP1"'
   // BASE_API: '"http://10.201.1.82:12345"'
 }
