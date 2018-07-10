d0621c0ba4009dc63fd2886ca8b2c4bc6bfa1054
jiangqixin <jiangqixin0525@126.com>
2018-07-10 10:03:15 +0800

    1

--- src/frame_src/components/ThemePicker/index.vue
+++ src/frame_src/components/ThemePicker/index.vue
@@ -37,7 +37,7 @@ export default {
     handleSetLanguage(style) {
       const themeCluster = this.getThemeCluster(style.replace('#', ''))
       const originalCluster = this.getThemeCluster(lastTheme.replace('#', ''))
-      var tagsColor = document.querySelector('.tags-view-wrapper .tags-view-item.active')
+      var tagsColor = document.querySelector('.tags-view-wrapper .tags-view-item')
       tagsColor.style.cssText = 'background-color:' + style + ';border-color:' + style
       var subMenu = document.querySelector('.sidebar-container .theme')
       if (style === '#3A8EE6') { // 蓝
