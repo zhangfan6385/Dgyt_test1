deff6297a98d1d4e3bfaf1bb93c9171b847942df
zhangshe <1312719913@qq.com>
2018-07-23 11:47:07 +0800

    增加云同步页面

--- configResult.js
+++
@@ -1,8 +0,0 @@
-import request from '@/frame_src/utils/request'
-export function GetConfigResult(query) { // 基础信息配置查询
-  return request({
-    url: 'SyncResult/fetchSyncResultList',
-    method: 'get',
-    params: query
-  })
-}
